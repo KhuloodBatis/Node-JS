d10eb9fb8f639c6372970900bd94429a821fc4e0
KhuloodBatis <k.batis@bimventures.com>
2022-07-26 16:17:51 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+//console.log("welcome to JavaScript Course");
+// let color = 'red'
+// console.log(color);
+// console.log(color);
+// console.log(color);
+// console.log(color);
+
+// let age = 25;
+// let yearOfBirth = 1957;
+// console.log(yearOfBirth)
+
+// let result = 2 > 3;
+// console.log(result);
+
+// let language = 'javaScript';
+// let message = `welcome to ${language}`;
+// console.log(message);
+
+// let name;
+// console.log(typeof name); //undefined
+
+// let number = 5.9;
+// console.log( number);
+
+let value = null;
+console.log(value);
